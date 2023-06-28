3aea6483b871483abd503a1a3fb0003afa09505f
Anna Samchenko <anna_ginz@list.ru>
2023-06-28 11:22:28 +0500

    Add HomeWork 12 (HerokuApp)

---
+++ HerokuApp/HerokuApp/HerokuAppTests/CheckBoxesTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace HerokuApp.HerokuAppTests
+{
+    public class CheckBoxesTests : BaseTest
+    {
+        [Test, Category("CheckBoxTest"), Description("Select checkBox")]
+        public void SelectCheckBox()
+        {
+            driver.FindElement(By.LinkText("Checkboxes")).Click();
+
+            List<IWebElement> checkBoxes = driver.FindElements(By.CssSelector("[type=checkbox]")).ToList();
+            Assert.IsNotEmpty(checkBoxes);
+
+            var checkBoxFirst = checkBoxes[0];
+            var checkBoxSecond = checkBoxes[1];
+
+            SetCheckBoxState(checkBoxFirst, false);
+            Assert.IsFalse(checkBoxFirst.Selected);
+
+            checkBoxFirst.Click();
+            Assert.IsTrue(checkBoxFirst.Selected);
+
+            SetCheckBoxState(checkBoxSecond, true);
+            Assert.IsTrue(checkBoxSecond.Selected);
+
+            checkBoxSecond.Click();
+            Assert.IsFalse(checkBoxSecond.Selected);
+        }
+
+        public void SetCheckBoxState(IWebElement element, bool flag)
+        {
+            var selected = element.Selected;
+            bool.TryParse(element.GetAttribute("checked"), out bool selectedByAttribute);
+
+            if ((selected || selectedByAttribute) != flag)
+            {
+                element.Click();
+            }
+        }
+    }
+}
---
+++ HerokuApp/HerokuApp/HerokuAppTests/TyposTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace HerokuApp.HerokuAppTests
+{
+    public class TyposTests : BaseTest
+    {
+        [Test, Category("TyposTest"), Description("Check text")]
+        [Retry(5)]
+        public void CheckText()
+        {
+            driver.FindElement(By.LinkText("Typos")).Click();
+
+            var expected = "Sometimes you'll see a typo, other times you won't.";
+            var atualText = driver.FindElement(By.XPath("(//p)[2]"));
+
+            Assert.That(atualText.Text, Is.EqualTo(expected));
+        }
+    }
+}
