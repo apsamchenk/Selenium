46d0f7359dcd9e82d487ad7f8780fbe19572f517
Anna Samchenko <anna_ginz@list.ru>
2023-06-29 16:11:48 +0500

    Add 3 new test classes for herokuapp (home work 14)

---
+++ HerokuApp/HerokuApp/HerokuAppTests/DynamicControlsTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HerokuApp.HerokuAppTests
+{
+    public class DynamicControlsTests : BaseTest
+    {
+        [Test, Category("DynamicControlsTest"), Description("Dynamic controls checkbox test")]
+        public void DynamicCheckBox()
+        {
+            driver.FindElement(By.LinkText("Dynamic Controls")).Click();
+            driver.FindElement(By.CssSelector("[type=checkbox]")).Click();
+            driver.FindElement(By.XPath("//*[@id='checkbox-example']/button")).Click();
+
+            WaitElement();
+
+            Assert.That(driver.FindElement(By.XPath("//*[@id='message']")).Text, Is.EqualTo("It's gone!"));
+            Assert.Throws<NoSuchElementException>(() => driver.FindElement(By.CssSelector("[type=checkbox]")));
+        }
+
+        [Test, Category("DynamicControlsTest"), Description("Dynamic controls input test")]
+        public void DynamicInput()
+        {
+            driver.FindElement(By.LinkText("Dynamic Controls")).Click();
+
+            Assert.IsFalse(driver.FindElement(By.XPath("//*[@id='input-example']/input")).Enabled);
+
+            driver.FindElement(By.XPath("//*[@id='input-example']/button")).Click();
+
+            WaitElement();
+
+            Assert.That(driver.FindElement(By.XPath("//*[@id='message']")).Text, Is.EqualTo("It's enabled!"));
+            Assert.IsTrue(driver.FindElement(By.XPath("//*[@id='input-example']/input")).Enabled);
+        }
+
+        public void WaitElement()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement dynamicElement = wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.XPath("//*[@id='message']"));
+            });
+        }
+    }
+}
