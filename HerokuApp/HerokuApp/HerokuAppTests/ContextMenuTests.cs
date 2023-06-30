e42dca64022c3fdca2940bc2323b230018d4d583
apsamchenk <129189673+apsamchenk@users.noreply.github.com>
2023-06-30 09:14:41 +0500

    Add 3 new test classes for herokuapp (home work 14) (#2)

---
+++ HerokuApp/HerokuApp/HerokuAppTests/ContextMenuTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace HerokuApp.HerokuAppTests
+{
+    public class ContextMenuTests : BaseTest
+    {
+        [Test, Category("ContextMenuTest"), Description("Context menu test")]
+        public void ContextMenu()
+        {
+            driver.FindElement(By.LinkText("Context Menu")).Click();
+
+            new Actions(driver)
+                .MoveToElement(driver.FindElement(By.Id("hot-spot")))
+                .ContextClick()
+                .Build()
+                .Perform();
+
+            Assert.That(driver.SwitchTo().Alert().Text, Is.EqualTo("You selected a context menu"));
+
+            driver.SwitchTo().Alert().Accept();
+        }
+    }
+}
---
+++ HerokuApp/HerokuApp/HerokuAppTests/FileUploadTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace HerokuApp.HerokuAppTests
+{
+    public class FileUploadTests : BaseTest
+    {
+        [Test, Category("FileUploadTest"), Description("File upload test")]
+        public void FileUpload()
+        {
+            driver.FindElement(By.LinkText("File Upload")).Click();
+
+            driver.FindElement(By.Id("file-upload")).SendKeys(Environment.CurrentDirectory + "\\HerokuAppTests\\TestData\\HomeWork.docx");
+
+            Assert.That(driver.FindElement(By.Id("file-upload")).GetAttribute("value"), Is.EqualTo("C:\\fakepath\\HomeWork.docx"));
+        }
+    }
+}
